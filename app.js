6285a7603b7535762e0f2fef44ab93f10f4072c6
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-05-30 08:39:22 +0200

    selectors: select all requered elements in js

---
+++ app.js
@@ -0,0 +1,5 @@
+// Selectors
+const taskInput = document.querySelector(".task-input input");
+const filters = document.querySelector(".filters span");
+const clearAll = document.querySelector(".clear-btn");
+const taskBox = document.querySelector(".task-box");
