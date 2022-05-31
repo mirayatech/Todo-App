ab2e6602f9d6ea2457940787c2061c7791e90f07
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-05-31 07:21:16 +0200

    todos (localstorage) show insidee the taskBox (new Li)

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ const taskBox = document.querySelector(".task-box");
 // Getting localstorage todo-list
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
+// Show the todos inside the taskBox (that are in the local storage)
+function showTodo() {
+  let li = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      li += `<li class="task">
+      <label for="${id}">
+          <input onclick="updateStatus(this)" type="checkbox" id="1" />
+          <p>${todo.name}</p>
+      </label>
+      <!-- settings -->
+      <div class="settings">
+          <i class="fa-solid fa-ellipsis"></i>
+          <ul class="task-menu">
+              <li><i class="fa-solid fa-pen"></i>Edit</li>
+              <li><i class="fa-regular fa-trash-can"></i>Delete</li>
+          </ul>
+      </div>
+  </li>`;
+    });
+  }
+
+  taskBox.innerHTML = li;
+}
+
+// Every ime we write and hit enter ,we add to the localstorage
 taskInput.addEventListener("keyup", (e) => {
   let userTask = taskInput.value.trim();
   if (e.key == "Enter" && userTask) {
@@ -18,5 +44,6 @@ taskInput.addEventListener("keyup", (e) => {
     let taskInfo = { name: userTask, status: "pending" };
     todos.push(taskInfo); //adding new task to todos
     localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
   }
 });
